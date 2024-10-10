34d6965975dec4adb2fdf9b1ec2a05e9ab5d73b0
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-10 15:57:28 -0400

    Installed npm, vite, and did vite config for .test domain

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "thirtydaystolearnlaravel.laracasts.test",
+        port: 8084,
+        hmr: {
+            host: "thirtydaystolearnlaravel.laracasts.test",
+            port: 8084,
+        },
+    },
 });
