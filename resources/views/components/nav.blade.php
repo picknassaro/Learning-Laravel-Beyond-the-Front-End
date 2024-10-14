b43364883a440341f02ca638e93e932da0b7ccfd
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-14 11:47:31 -0400

    Created basic nested component that will be extended later to be more useful

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,5 @@
 <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
+    <x-nav-link href="/">Home</x-nav-link>
+    <x-nav-link href="/about">About</x-nav-link>
+    <x-nav-link href="/contact">Contact</x-nav-link>
 </nav>
