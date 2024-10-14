efc47ab4f75ee79cb5ef9b4383578a3e475417be
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-14 10:59:11 -0400

    Renamed welcome view file to home and learned about things that can be done with routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Return a view
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// Return a string
+Route::get('/about', function () {
+    return "About Page";
+});
+
+// API example
+Route::get('/api', function () {
+    return ["foo" => "bar"];
+});
+
+// Redirect to another route
+Route::get('/home', function () {
+    return redirect('/');
+});
+
+// Give a route a name
+Route::get('/login', function () {
+    return "Login page";
+})->name('login');
+
+// Redirect to a named route
+Route::get('/dashboard', function () {
+    return redirect()->route('login');
 });
