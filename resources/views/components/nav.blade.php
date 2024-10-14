e838de9496dff0e0ed5c0c708c88514f3e3f3ff4
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-14 14:30:23 -0400

    Refactored the nav component to not have repeated code. Moved repeated code into nav-link.

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,8 +1,5 @@
 <div>
-    <x-nav-link href="/"
-        class="block md:inline rounded-md px-3 py-2 text-base md:text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</x-nav-link>
-    <x-nav-link href="/about"
-        class="block md:inline rounded-md px-3 py-2 text-base md:text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</x-nav-link>
-    <x-nav-link href="/contact"
-        class="block md:inline rounded-md px-3 py-2 text-base md:text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</x-nav-link>
+    <x-nav-link routeName="home">Home</x-nav-link>
+    <x-nav-link routeName="about">About</x-nav-link>
+    <x-nav-link routeName="contact">Contact</x-nav-link>
 </div>
