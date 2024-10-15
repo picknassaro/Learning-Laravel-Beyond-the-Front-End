18a32918d57724ea4a7d41fa948af9a442876e19
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-15 14:56:19 -0400

    Self-directed learning: Updated User factory and created JobListing Factory

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 
 class JobListing extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'description', 'location', 'type', 'salary'];
 }
---
+++ database/factories/JobListingFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\JobListing>
+ */
+class JobListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle(),
+            'description' => fake()->paragraph(),
+            'location' => fake()->randomElement([fake()->city(), 'Remote']),
+            'type' => fake()->randomElement(['Full-time', 'Part-time']),
+            'salary' => fake()->numerify('$###,###.##'),
+        ];
+    }
+}
