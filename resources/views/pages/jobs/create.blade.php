779d4632c81dcdfbd058281ad9888e6b764f78b0
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-16 16:59:24 -0400

    Fixed stuff in the job post form

--- resources/views/pages/jobs/create.blade.php
+++ resources/views/pages/jobs/create.blade.php
@@ -9,31 +9,31 @@
         @csrf
         <div class="w-full mb-4">
             <label class="w-full block mb-1" for="title">Title</label>
-            <input class="rounded-xl w-full h-12 p-4 focus:outline-blue-500" type="text" name="title" placeholder="" />
+            <input class="py-2 px-4 rounded w-full h-12 focus:outline-blue-500" type="text" name="title" placeholder="" />
         </div>
         <div class="w-full mb-4">
             <label class="w-full block mb-1" for="description">Description</label>
-            <div class="rounded-xl bg-white p-1 focus-within:outline outline-blue-500">
-                <textarea class="w-full rounded-xl h-48 p-3 focus:outline-none" name="description"></textarea>
+            <div class="rounded bg-white p-1 focus-within:outline outline-blue-500">
+                <textarea class="w-full rounded h-48 p-3 focus:outline-none" name="description"></textarea>
             </div>
         </div>
         <div class="w-full flex mb-4">
             <div class="grow flex flex-col mr-4">
                 <label class="block mb-1" for="location">Location</label>
-                <input class="rounded-xl h-12 p-4 focus:outline-blue-500" type="text" name="location" placeholder="" />
+                <input class="py-2 px-4 rounded h-12 focus:outline-blue-500" type="text" name="location" placeholder="" />
             </div>
             <div class="grow flex flex-col mr-4">
                 <label class="block mb-1" for="type">Type</label>
-                <select class="rounded-xl h-12 p-4 focus:outline-blue-500 bg-white" name="type" placeholder="">
+                <select class="py-2 px-4 rounded h-12 focus:outline-blue-500 bg-white" name="type" placeholder="">
                     <option value="full-time">Full Time</option>
                     <option value="part-time">Part Time</option>
                 </select>
             </div>
             <div class="grow flex flex-col">
                 <label class="block mb-1" for="salary">Salary</label>
-                <input class="rounded-xl h-12 p-4 focus:outline-blue-500" type="text" name="salary" placeholder="" />
+                <input class="py-2 px-4 rounded h-12 focus:outline-blue-500" type="text" name="salary" placeholder="" />
             </div>
         </div>
-        <button class="rounded-xl p-4 bg-blue-500 text-white">Post Job</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Post Job</button>
     </form>
 @endsection
