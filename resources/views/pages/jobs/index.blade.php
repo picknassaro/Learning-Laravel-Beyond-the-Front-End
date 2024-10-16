f653d8d42c8c8c37dae676d55d3fb4479575e87b
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-16 16:59:53 -0400

    Added button style link to create jobs page

--- resources/views/pages/jobs/index.blade.php
+++ resources/views/pages/jobs/index.blade.php
@@ -5,7 +5,10 @@
 @section('page-header', 'Jobs')
 
 @section('content')
-    <h2 class="mb-4">Available Jobs:</h2>
+    <div class="flex justify-between">
+        <h2 class="mb-4">Available Jobs:</h2>
+        <a href="jobs/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Post a Job</a>
+    </div>
     <ul>
         @foreach ($jobs as $job)
             <li class="list-disc mb-4">
