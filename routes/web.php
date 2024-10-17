a5889f692144dd8923036463028e9bef24aec250
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-17 15:02:39 -0400

    Learned about route model binding

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,8 @@ Route::get('/contact', function () {
 
 
 
-// Job routes
-// Show all
-// with('employer') is not for allowing us to show the employers. That will work regardless. It is an eager loading method that prevent an N+1 query problem. It will load all the employers in one query instead of loading them one by one.
+// Job Listing routes
+// Show all Job Listings
 Route::get('/jobs', function () {
     $jobs = JobListing::with('employer')->latest()->simplePaginate(10);
     return view('pages.jobs.index', [
@@ -33,25 +32,30 @@ Route::get('/jobs', function () {
     ]);
 })->name('showAllJobs');
 
-// Post a Job
+// ↑↑ with('employer') is not for allowing us to show the employers. That will work regardless. It is an eager loading method that prevent an N+1 query problem. It will load all the employers in one query instead of loading them one by one.
+
+// Load the page where we fill out a form to post a Job Listing
 Route::get('/jobs/create', function () {
     return view('pages.jobs.create');
 })->name('createJob');
 
-// View single Job
-Route::get('/jobs/{id}', function ($id) {
-    $job = JobListing::find($id);
+// View a Single Listing
+Route::get('/jobs/{job}', function (JobListing $job) {
     return view('pages.jobs.show', ['job' => $job]);
 })->name('showSingleJob');
 
-// Edit single Job
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = JobListing::find($id);
+/* ↑↑
+get('/jobs/{job}') is a route model bindig. function (JobListing $job) is a type hinting that tells Laravel to fetch the JobListing model with the id of {job} and pass it to the closure. If the url is /jobs/1, Laravel will get a result from the JobListing model where the id is 1 and pass it to the closure as $job.
+['job' => $job] inside the view() method is an array that passes the $job variable to the view.
+*/
+
+// View the form where we can edit or delete a Job Listing
+Route::get('/jobs/{job}/edit', function (JobListing $job) {
     return view('pages.jobs.edit', ['job' => $job]);
 })->name('editJob');
 
-// Store a Job
-Route::post('/jobs', function () {
+// take what was entered into the form on the createJob page and store it in the database
+Route::post('/jobs', function (JobListing $job) {
     request()->validate([
         'title' => ['required'],
         'description' => ['required'],
@@ -59,7 +63,7 @@ Route::post('/jobs', function () {
         'type' => ['required'],
         'salary' => ['required', 'regex:/^\$((\d{1,3})(,\d{3})*|\d+)(\.\d{2})?$/']
     ]);
-    JobListing::create([
+    $job::create([
         'title' => request('title'),
         'description' => request('description'),
         'location' => request('location'),
@@ -70,8 +74,8 @@ Route::post('/jobs', function () {
     return redirect()->route('showAllJobs');
 })->name('storeJob');
 
-// Edit a Job
-Route::patch('/jobs/{id}', function ($id) {
+// We go here when we click submit in the patching version of the Job Listing form
+Route::patch('/jobs/{job}', function (JobListing $job) {
     request()->validate([
         'title' => ['required'],
         'description' => ['required'],
@@ -79,19 +83,19 @@ Route::patch('/jobs/{id}', function ($id) {
         'type' => ['required'],
         'salary' => ['required', 'regex:/^\$((\d{1,3})(,\d{3})*|\d+)(\.\d{2})?$/']
     ]);
-    JobListing::findOrFail($id)->update([
+    $job->update([
         'title' => request('title'),
         'description' => request('description'),
         'location' => request('location'),
         'type' => request('type'),
         'salary' => preg_match('/\.\d{2}$/', request('salary')) ? request('salary') : request('salary') . '.00',
     ]);
-    return redirect()->route('showSingleJob', ['id' => $id]);
+    return redirect()->route('showSingleJob', ['job' => $job->id]);
 })->name('editSingleJob');
 
-// Delete a Job
-Route::delete('/jobs/{id}', function ($id) {
-    JobListing::findOrFail($id)->delete();
+// We go here when we click delete in the patching version of the Job Listing form
+Route::delete('/jobs/{job}', function (JobListing $job) {
+    $job->delete();
     return redirect()->route('showAllJobs');
 })->name('deleteSingleJob');
 
