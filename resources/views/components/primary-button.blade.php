f36663c50d4df9973616dea7724b47e9307bf308
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-19 10:43:24 -0400

    Added an option to make the primary button look like a link instead of a button

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,13 +1,11 @@
-@php
-    // This is a component just for buttons and links that need to look like buttons. This is not for links that need to look like links or buttons that need to look like links.
-@endphp
+@props(['type', 'href', 'linkAppearance' => null])
 
 @php
     $classes = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-start h-13';
 @endphp
 
 @if ($type === 'submit')
-    <button {{ $attributes->merge(['type' => 'submit', 'class' => $classes]) }}>
+    <button {{ $attributes->merge(['type' => 'submit', 'class' => $linkAppearance === null ?? $classes]) }}>
         {{ $slot }}
     </button>
 @elseif ($type === 'link')
