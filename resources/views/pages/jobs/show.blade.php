ed3935d84a0e31364f57648c46f95fe61fa4c437
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-24 18:52:32 -0400

    Fixed code duplication and made it so that the job poster and the admin see who applied to a job in the showSingleJob route, but no one else ever can. Also made it so that related jobs only show to guests and those who are logged in but neither the job poster nor the admin

--- resources/views/pages/jobs/show.blade.php
+++ resources/views/pages/jobs/show.blade.php
@@ -21,36 +21,31 @@
 
 @section('content')
     <div class="mb-8 md:flex md:flex-wrap md:justify-center xl:grid xl:grid-cols-[48rem_1fr] xl:gap-8">
-        <div class="mb-8 flex justify-center 2xl:mb-0">
+        <div class="mb-8 flex justify-center xl:mb-0">
             <x-job-card :job="$job"
                         cardSize="normal"
                         strLimit="null" />
         </div>
-        <div class="md:hidden">
-            <h2 class="mb-4 text-xl font-semibold">Other Jobs from the same Employer</h2>
-            <div class="grid grid-rows-3 gap-8">
-                @foreach ($relatedJobs as $relatedJob)
-                    <x-job-card :job="$relatedJob"
-                                cardSize="mini" />
+        <div class="md:block md:max-w-5xl">
+            @if (!Auth::check() || (Auth::user()->cannot('edit-job', $job) && Auth::user()->cannot('is-admin', $job)))
+                <h2 class="mb-4 text-xl font-semibold">Other Jobs from the same Employer</h2>
+                <div class="grid grid-cols-1 grid-rows-3 gap-8 md:grid-cols-2 md:grid-rows-2 xl:grid-cols-1 xl:grid-rows-3">
+                    @foreach ($relatedJobs as $relatedJob)
+                        <x-job-card :job="$relatedJob"
+                                    cardSize="mini" />
+                    @endforeach
+                </div>
+            @endif
+            @if (
+                $job->users->isNotEmpty() &&
+                    Auth::check() &&
+                    (Auth::user()->can('edit-job', $job) || Auth::user()->can('is-admin', $job)))
+                <h2 class="mb-4 text-xl font-semibold">Latest applicants:</h2>
+                @foreach ($job->users as $user)
+                    <span
+                          class="mb-4">{{ $user->first_name[0] . '. ' . $user->last_name }}{{ !$loop->last ? ', ' : '' }}</span>
                 @endforeach
-            </div>
+            @endif
         </div>
-        <div class="hidden md:block md:max-w-5xl">
-            <h2 class="mb-4 text-xl font-semibold">Other Jobs from the same Employer</h2>
-            <div class="grid grid-cols-2 grid-rows-2 gap-8 xl:grid xl:grid-cols-1 xl:grid-rows-3">
-                @foreach ($relatedJobs as $relatedJob)
-                    <x-job-card :job="$relatedJob"
-                                cardSize="mini" />
-                @endforeach
-            </div>
-        </div>
-    </div>
-    <div>
-        @if ($job->users->isNotEmpty())
-        <h2 class="mb-4 text-xl font-semibold">Latest applicants:</h2>
-            @foreach ($job->users as $user)
-                <span class="mb-4">{{ $user->first_name[0] . '. ' . $user->last_name }}{{ !$loop->last ? ', ' : '' }}</span>
-            @endforeach
-        @endif
     </div>
 @endsection
