32a3724f06ad1fca391af455ebf103cca5c4ec8f
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-25 17:22:49 -0400

    Fixed form focus CSS

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,6 +1,6 @@
 @props(['full' => 'false', 'type' => '', 'name' => '', 'outline' => 'false', 'resource' => null])
 
-<input class="{{ $full === 'true' ? 'w-full' : '' }} {{ $outline === 'true' ? 'border-2 focus:border-none border-gray-400' : '' }} h-12 rounded px-4 py-2 focus:outline-blue-500"
+<input class="{{ $full === 'true' ? 'w-full' : '' }} {{ $outline === 'true' ? 'border-2 focus:border-none border-gray-400' : '' }} h-12 rounded px-4 py-2 focus:border-blue-500"
        type="{{ $type }}"
        name="{{ $name }}"
        value="{{ old($name, $resource->$name ?? '') }}" />
--- resources/views/components/form-select.blade.php
+++ resources/views/components/form-select.blade.php
@@ -1,6 +1,6 @@
 @props(['full' => 'false', 'name' => '', 'options' => [], 'resource' => null])
 
-<select class="{{ $full === 'true' ? 'w-full' : '' }} h-12 rounded bg-white px-4 py-2 focus:outline-blue-500"
+<select class="{{ $full === 'true' ? 'w-full' : '' }} h-12 rounded bg-white px-4 py-2 focus:border-blue-500"
         name="{{ $name }}">
     @foreach ($options as $option)
         <option value="{{ $option }}"
