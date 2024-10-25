53b4b38eea3ad0bed160349b66924545f66744c4
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-25 10:35:18 -0400

    Fixed a possible empty div

--- resources/views/pages/jobs/show.blade.php
+++ resources/views/pages/jobs/show.blade.php
@@ -20,18 +20,19 @@
 @endsection
 
 @php
-    $jobHasApplicants = $job->users->isNotEmpty()
+    $jobHasApplicants = $job->users->isNotEmpty();
 @endphp
 
 @section('content')
-    <div class="{{ $jobHasApplicants === true ? 'mb-8 md:flex md:flex-wrap md:justify-center xl:grid xl:grid-cols-[48rem_1fr] xl:gap-8' : '' }}">
+    <div
+         class="{{ $jobHasApplicants === true ? 'mb-8 md:flex md:flex-wrap md:justify-center xl:grid xl:grid-cols-[48rem_1fr] xl:gap-8' : '' }}">
         <div class="mb-8 flex justify-center xl:mb-0">
             <x-job-card :job="$job"
                         cardSize="normal"
                         strLimit="null" />
         </div>
-        <div class="md:block md:max-w-5xl">
-            @if (!Auth::check() || (Auth::user()->cannot('edit-job', $job) && Auth::user()->cannot('is-admin', $job)))
+        @if (!Auth::check() || (Auth::user()->cannot('edit-job', $job) && Auth::user()->cannot('is-admin', $job)))
+            <div class="md:block md:max-w-5xl">
                 <h2 class="mb-4 text-xl font-semibold">Other Jobs from the same Employer</h2>
                 <div class="grid grid-cols-1 grid-rows-3 gap-8 md:grid-cols-2 md:grid-rows-2 xl:grid-cols-1 xl:grid-rows-3">
                     @foreach ($relatedJobs as $relatedJob)
@@ -39,14 +40,19 @@
                                     cardSize="mini" />
                     @endforeach
                 </div>
-            @endif
-            @if ($jobHasApplicants === true && Auth::check() && (Auth::user()->can('edit-job', $job) || Auth::user()->can('is-admin', $job)))
+            </div>
+        @endif
+        @if (
+            $jobHasApplicants === true &&
+                Auth::check() &&
+                (Auth::user()->can('edit-job', $job) || Auth::user()->can('is-admin', $job)))
+            <div class="md:block md:max-w-5xl">
                 <h2 class="mb-4 text-xl font-semibold">Latest applicants:</h2>
                 @foreach ($job->users as $user)
                     <span
                           class="mb-4">{{ $user->first_name[0] . '. ' . $user->last_name }}{{ !$loop->last ? ', ' : '' }}</span>
                 @endforeach
-            @endif
-        </div>
+            </div>
+        @endif
     </div>
 @endsection
