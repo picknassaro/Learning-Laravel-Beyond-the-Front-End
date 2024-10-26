64a74b6219ffd774c5b97fc35780d4543b8c65ff
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-26 00:24:06 -0400

    Finished removing things that were beyond the scope of the tutorial

--- resources/views/mail/job-listing-posted.blade.php
+++
@@ -1,6 +0,0 @@
-Your job listing has been posted successfully!<br />
-<br />
-<a href="{{ url('/jobs/' . $job->id) }}">Check it out</a><br />
-<br />
-Thanks,<br>
-{{ config('app.name') }}
--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -15,29 +15,7 @@
 @endsection
 
 @section('content')
-    <div class="max-w-screen-sm block mx-auto lg:max-w-none md:grid md:grid-cols-1 md:gap-8 md:pb-8 lg:grid-cols-2 xl:grid-cols-3">
-        <div class="lg:border-r-2 lg:border-gray-800 lg:pr-8 xl:col-span-2 xl:grid-cols-2">
-            <h2 class="mb-4 text-3xl font-semibold">Your recent job listings</h2>
-            <ul class="grid grid-cols-1 gap-8 pb-8 xl:grid-cols-2">
-                @foreach ($jobs as $job)
-                    <x-job-card :job="$job" />
-                @endforeach
-            </ul>
-            <div class="mb-4 grid grid-cols-2">
-                <div class="flex justify-start">
-                    @if ($jobs->previousPageUrl())
-                        <x-primary-button type="buttonLink"
-                                          href="{{ $jobs->previousPageUrl() }}">Previous</x-primary-button>
-                    @endif
-                </div>
-                <div class="flex justify-end">
-                    @if ($jobs->nextPageUrl())
-                        <x-primary-button type="buttonLink"
-                                          href="{{ $jobs->nextPageUrl() }}">Next</x-primary-button>
-                    @endif
-                </div>
-            </div>
-        </div>
+    <div class="max-w-screen-sm block mx-auto md:grid md:grid-cols-1 md:gap-8 md:pb-8">
         <div class="pt-12 md:pt-8 lg:pt-0">
             <x-auth-form type="updateProfile"
                          :user="$user" />
